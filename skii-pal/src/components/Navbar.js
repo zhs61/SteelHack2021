4dbef2a123e0c60b2415bda0f1c689cf80889e0e
Xiaoxin_He <xih85@pitt.edu>
2021-02-20 04:39:20 -0500

    fix nav bar layout without white underscore

--- skii-pal/src/components/Navbar.js
+++ skii-pal/src/components/Navbar.js
@@ -30,7 +30,7 @@ function Navbar() {
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
             SKI  
-            <i class='fas fa-skiing' />
+            <i className='fas fa-skiing' />
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
