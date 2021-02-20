a0bd7bb71817aa1e9a3eb111ec0cfe6eddf9dba2
Zhongxuan Song <zhs61@pitt.edu>
2021-02-19 22:27:19 -0500

    added the auth

--- skii-pal/src/App.js
+++ skii-pal/src/App.js
@@ -1,10 +1,30 @@
 import "./App.css";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "./contexts/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import PrivateRoute from "./components/PrivateRoute";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="app">
-      <h1>Home</h1>
-    </div>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <PrivateRoute exact path="/" component={Home} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
   );
 }
 
