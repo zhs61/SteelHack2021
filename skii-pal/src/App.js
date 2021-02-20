0d1d21e7ade99e8167fad2f44fe2abe7e1297974
Zhixiang Liu <43303332+Jasonliu-pitt@users.noreply.github.com>
2021-02-20 01:11:29 -0500

    add redux

--- skii-pal/src/App.js
+++ skii-pal/src/App.js
@@ -1,11 +1,42 @@
+import React, { useEffect, lazy, Suspense } from 'react';
+import { Switch, Route} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectCurrentUser } from './redux/user/user.selectors';
+import { checkUserSession } from './redux/user/user.actions';
+
+
+
 import "./App.css";
 
-function App() {
+const HomePage = lazy(() => import('./pages/homepage/homepage.component'));
+
+const App = ({ checkUserSession, currentUser }) => {
+  useEffect(() => {
+    checkUserSession();
+  }, [checkUserSession]);
+
   return (
-    <div className="app">
-      <h1>Home</h1>
+    <div>
+      <Switch>
+          <Suspense fallback={<div />}>
+            <Route exact path='/' component={HomePage} />
+          </Suspense>
+      </Switch>
     </div>
   );
-}
+};
 
-export default App;
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
+});
+
+const mapDispatchToProps = dispatch => ({
+  checkUserSession: () => dispatch(checkUserSession())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
---
+++ skii-pal/src/redux/root-reducer.js
@@ -0,0 +1,19 @@
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+import userReducer from './user/user.reducer';
+import directoryReducer from './directory/directory.reducer';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['']
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  directory: directoryReducer
+});
+
+export default persistReducer(persistConfig, rootReducer);
