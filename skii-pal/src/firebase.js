5d04a0f772607b321f3a5d8b0f0246c48fa177b2
Zhongxuan Song <zhs61@pitt.edu>
2021-02-19 22:01:41 -0500

    added firebase

---
+++ skii-pal/src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+import "firebase/auth";
+
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyCS4_e9d9u0Cn75l9NgFoMmXXLSFTurbQ0",
+  authDomain: "skii-pal.firebaseapp.com",
+  projectId: "skii-pal",
+  storageBucket: "skii-pal.appspot.com",
+  messagingSenderId: "373520097652",
+  appId: "1:373520097652:web:c67917bee57640bfc84c21",
+});
+
+export const auth = app.auth();
+export default app;
