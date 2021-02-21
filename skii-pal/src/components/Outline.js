8251d318f4f71b61d985c959d922a889299b6706
Zhongxuan Song <zhs61@pitt.edu>
2021-02-21 00:50:47 -0500

    Update outline.js

--- skii-pal/src/components/Outline.js
+++ skii-pal/src/components/Outline.js
@@ -1,43 +1,72 @@
-import * as React from 'react';
-import './Outline.css';
-import { makeStyles } from '@material-ui/core/styles';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { AiFillStar } from 'react-icons/ai';
-import { useSelector } from 'react-redux';
+import * as React from "react";
+import "./Outline.css";
+import { makeStyles } from "@material-ui/core/styles";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { AiFillStar } from "react-icons/ai";
+import { useSelector } from "react-redux";
+import Modal from "@material-ui/core/Modal";
+import { FaSkiing } from "react-icons/fa";
+import { BiHappyBeaming } from "react-icons/bi";
 
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
 
-const useStyles = makeStyles({
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
   root: {
     maxWidth: 345,
   },
   media: {
     height: 140,
   },
-});
+}));
 
 const Outline = ({ outline }) => {
-  const data = useSelector(state => state.outline);
-  // if (outline === 'outline loading...') {
-  //   return (
-  //     <div className="outline">
-  //       <CircularProgress />
-  //     </div>
-  //   );
-  // }
-
+  const data = useSelector((state) => state.outline);
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
   const classes = useStyles();
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const body = (
+    <div style={modalStyle} className={classes.paper}>
+      <h4 id="simple-modal-title">
+        Successfully Join The Team <FaSkiing /> <BiHappyBeaming />
+      </h4>
+    </div>
+  );
+
   return (
     <div className="outline">
-      {/* <h4 className="placeholder">{!!outline ? null : 'Spresso Text-Outline\n v.1.0.0'}</h4> */}
       <h3>{!outline ? null : outline.title}</h3>
       <p>{!outline ? null : outline.text}</p>
 
       <CardActionArea>
-
         <CardMedia
           style={{
             height: "40px",
@@ -45,7 +74,7 @@ const Outline = ({ outline }) => {
             paddingLeft: "56.25%",
             paddingTop: "56.25%", // 16:9,
             marginTop: "20px",
-            width: "30px"
+            width: "30px",
           }}
           className={classes.media}
           image={data.Image || "images/img-1.jpg"}
@@ -54,11 +83,10 @@ const Outline = ({ outline }) => {
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
             {data.SKI_group_name || "SKI team A"}
-
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-            {data.summary || "Odio aenean sed adipiscing diam. Placerat in egestas erat imperdiet sed euismod. Egestas congue quisque egestas diam in arcu cursus euismod quis. Enim nunc faucibus a pellentesque. Magna etiam tempor orci eu lobortis elementum nibh. Quam adipiscing vitae proin sagittis nisl rhoncus. Eget est lorem ipsum dolor sit amet consectetur. Nec ullamcorper sit amet risus nullam. Facilisis sed odio morbi quis commodo odio aenean sed adipiscing. Scelerisque varius morbi enim nunc. In nulla posuere sollicitudin aliquam ultrices sagittis. Duis tristique sollicitudin nibh sit amet commodo. Quis hendrerit dolor magna eget est lorem ipsum. In ornare quam viverra orci sagittis eu volutpat odio facilisis."}
-
+            {data.summary ||
+              "Odio aenean sed adipiscing diam. Placerat in egestas erat imperdiet sed euismod. Egestas congue quisque egestas diam in arcu cursus euismod quis. Enim nunc faucibus a pellentesque. Magna etiam tempor orci eu lobortis elementum nibh. Quam adipiscing vitae proin sagittis nisl rhoncus. Eget est lorem ipsum dolor sit amet consectetur. Nec ullamcorper sit amet risus nullam. Facilisis sed odio morbi quis commodo odio aenean sed adipiscing. Scelerisque varius morbi enim nunc. In nulla posuere sollicitudin aliquam ultrices sagittis. Duis tristique sollicitudin nibh sit amet commodo. Quis hendrerit dolor magna eget est lorem ipsum. In ornare quam viverra orci sagittis eu volutpat odio facilisis."}
           </Typography>
           <br />
           <AiFillStar />
@@ -68,13 +96,25 @@ const Outline = ({ outline }) => {
       </CardActionArea>
       <br />
 
-
-      <Button className='btns' buttonStyle='btn--submmit' color="primary" variant="contained" >
-        Payment Now
-    </Button>
-
+      <Button
+        className="btns"
+        buttonStyle="btn--submmit"
+        color="primary"
+        variant="contained"
+        onClick={handleOpen}
+      >
+        Join
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        {body}
+      </Modal>
     </div>
   );
 };
 
-export default Outline
+export default Outline;
