72644d5e6c6ff619a1806f2618afe446ab5e636c
Zhongxuan Song <zhs61@pitt.edu>
2021-02-20 23:10:40 -0500

    add data select in outline

--- skii-pal/src/components/Outline.js
+++ skii-pal/src/components/Outline.js
@@ -1,10 +1,11 @@
-
-import * as React from 'react';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import './Outline.css';
+import * as React from "react";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import "./Outline.css";
+import { useSelector } from "react-redux";
 
 const Outline = ({ outline }) => {
-  if (outline === 'outline loading...') {
+  const data = useSelector((state) => state.outline);
+  if (outline === "outline loading...") {
     return (
       <div className="outline">
         <CircularProgress />
@@ -13,11 +14,13 @@ const Outline = ({ outline }) => {
   }
   return (
     <div className="outline">
-      <h4 className="placeholder">{!!outline ? null : 'Spresso Text-Outline\n v.1.0.0'}</h4>
+      <h4 className="placeholder">
+        {!!outline ? null : "Spresso Text-Outline\n v.1.0.0"}
+      </h4>
       <h3>{!outline ? null : outline.title}</h3>
       <p>{!outline ? null : outline.text}</p>
     </div>
   );
 };
 
-export default Outline
+export default Outline;
