aa37190a9a6a4544d0f73dec64e57255369550e7
Xiaoxin_He <xih85@pitt.edu>
2021-02-21 04:22:31 -0500

    a

--- skii-pal/src/App.js
+++ skii-pal/src/App.js
@@ -1,27 +1,30 @@
-import React, { useEffect, lazy, Suspense } from "react";
-import { Switch, Route } from "react-router-dom";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
+
+import React, {useEffect, lazy, Suspense} from "react";
+import {Switch, Route} from "react-router-dom";
+import {connect} from "react-redux";
+import {createStructuredSelector} from "reselect";
 import Navbar from "./components/Navbar";
 import CircularProgress from "@material-ui/core/CircularProgress";
-import { selectCurrentUser } from "./redux/user/user.selectors";
-import { checkUserSession } from "./redux/user/user.actions";
+import {selectCurrentUser} from "./redux/user/user.selectors";
+import {checkUserSession} from "./redux/user/user.actions";
+
 
 import "./App.css";
-import { AuthProvider } from "./contexts/AuthContext";
+import {AuthProvider} from "./contexts/AuthContext";
 import PrivateRoute from "./components/PrivateRoute";
 
-const Home = lazy(() => import("./components/pages/Home"));
-const Search = lazy(() => import("./components/pages/Search"));
-const Signup  = lazy(() => import( "./components/Signup"));
-const Login  = lazy(()=> import( "./components/Login"));
-const Profile = lazy(() => import( "./components/pages/Profile"));
+const Home=lazy( () => import( "./components/pages/Home" ) );
+const Search=lazy( () => import( "./components/pages/Search" ) );
+const Signup=lazy( () => import( "./components/Signup" ) );
+const Login=lazy( () => import( "./components/Login" ) );
+const Profile=lazy( () => import( "./components/pages/Profile" ) );
+const Discussion=lazy( () => import( './components/pages/Discussion' ) );
 
 
-const App = ({ checkUserSession, currentUser }) => {
-  useEffect(() => {
+const App=( {checkUserSession, currentUser} ) => {
+  useEffect( () => {
     checkUserSession();
-  }, [checkUserSession]);
+  }, [ checkUserSession ] );
 
   return (
     <div>
@@ -34,6 +37,7 @@ const App = ({ checkUserSession, currentUser }) => {
             <Route path="/login" component={Login} />
             <PrivateRoute exact path="/profile" component={Profile} />
             <Route exact path="/search" component={Search} />
+            <Route path='/discussion' component={Discussion} />
           </Suspense>
         </Switch>
       </AuthProvider>
@@ -41,12 +45,14 @@ const App = ({ checkUserSession, currentUser }) => {
   );
 };
 
-const mapStateToProps = createStructuredSelector({
+
+const mapStateToProps=createStructuredSelector( {
   currentUser: selectCurrentUser,
-});
+} );
 
-const mapDispatchToProps = (dispatch) => ({
-  checkUserSession: () => dispatch(checkUserSession()),
-});
+const mapDispatchToProps=( dispatch ) => ( {
+  checkUserSession: () => dispatch( checkUserSession() ),
+} );
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect( mapStateToProps, mapDispatchToProps )( App );
+
